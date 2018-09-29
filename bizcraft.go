c924f8cefecd0e46e4e334158d64382658c4a6c7
jasjohncom <john@jasjohn.com>
2018-09-29 20:41:58 +1000

    Initial commit with a log message

---
+++ bizcraft.go
@@ -0,0 +1,8 @@
+package main
+
+import "bizcraft/logs"
+
+func main() {
+
+	logs.Log.Info("Starting bizcraft. Please wait...")
+}
---
+++ logs/logs.go
@@ -0,0 +1,14 @@
+package logs
+
+import (
+	"github.com/op/go-logging"
+)
+
+var (
+	logFormat = "%{color}[%{level:.4s}] %{time:15:04:05.000000} %{id:06x} [%{shortpkg}] %{longfunc} -> %{color:reset}%{message}"
+	Log       = logging.MustGetLogger("hercules")
+)
+
+func Start() {
+	logging.SetFormatter(logging.MustStringFormatter(logFormat))
+}
